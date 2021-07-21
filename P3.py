61ccf60b0699e45b81b81a4481407564a9747b8d
BhaktiBudar <73065740+BhaktiBudar@users.noreply.github.com>
2021-07-21 19:38:13 +0530

    first commit

---
+++ P3.py
@@ -0,0 +1,10 @@
+#wap to swap two numbers
+
+n1 = int(input("Enter first integer "))
+n2 = int(input("Enter second integer "))
+
+print("n1 = ", n1, "n2 = ", n2)
+
+n1, n2 = n2, n1
+
+print("n1 = ", n1, "n2 = ", n2)
